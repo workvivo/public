4a741f6f56df0ceeac50b64698ed070b165db1c4
Darragh Duffy <Darraghduffy@me.com>
2025-07-07 15:20:21 +0100

    use existing registered keys. update to make consistent across code examples.

--- JWT/One-time-passcode/PHP/GenerateJWT.php
+++ JWT/One-time-passcode/PHP/GenerateJWT.php
@@ -1,62 +1,76 @@
 <?php
 
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__.'/vendor/autoload.php';
 use Firebase\JWT\JWT;
 
-// === CONFIGURATION ===
-
-$privKeyFile = __DIR__ . '/private.pem';
-$pubKeyFile  = __DIR__ . '/public.pem';
-
-// === USER NEEDS TO CONFIGURE THESE SETTINGS HERE ===
-$orgId       = 165; // This is the organisation ID
-$appWorkvivo = '3981'; // APP ID from your Workvivo app with unwiredotp.*.write permission
-$appAud      = 'unwired.workvivo.red';
-$userEmail   = 'test@nomail';
-
-$jwtLifetime = 600;
-$apiUrl      = 'https://api-gateway.workvivo.red/v1/unwired/users/otp'; //In case it is EU Production HOST should be api.workvivo.com
-$appIssuer   = 'org1';
-$appSubject  = 'app';
-
-// === KEYPAIR GENERATION OR LOADING ===
+/**
+ * You will need to generate a public/private key pair for your application
+ * the following Keys are should only be used for testing purposes and should not be used in production.
+ */
+$privKeyFile = __DIR__.'/../Keys/private.pem';
+$pubKeyFile = __DIR__.'/../Keys/public.pem';
+$jwksFile = __DIR__.'/../Keys/jwks.json';
 
 /*
- * You can generate your own RSA keypair manually using OpenSSL:
- *
- * # Generate a 4096-bit private key
- * openssl genpkey -algorithm RSA -out private.pem -pkeyopt rsa_keygen_bits:4096
- *
- * # Extract the public key from the private key
- * openssl rsa -in private.pem -pubout -out public.pem
- *
- * Place both files (private.pem and public.pem) in the same directory as this script.
- * The script will use them automatically instead of generating new keys.
- */
+   The Organisation ID is the ID of the Workvivo organisation.
+*/
 
-if (! file_exists($privKeyFile) || ! file_exists($pubKeyFile)) {
-    // generate a new 4096-bit RSA keypair
-    $res = openssl_pkey_new([
-        'private_key_bits' => 4096,
-        'private_key_type' => OPENSSL_KEYTYPE_RSA,
-    ]);
-    openssl_pkey_export($res, $privatePem);
-    $keyDetails   = openssl_pkey_get_details($res);
-    $publicPem    = $keyDetails['key'];
+$orgId = 165;
 
-    // save to file
-    file_put_contents($privKeyFile, $privatePem);
-    file_put_contents($pubKeyFile,  $publicPem);
+/*
+    The App ID of the Workvivo app with unwiredotp.*.write permission.
+*/
 
-    echo "Generated and saved new RSA keypair.\n";
-} else {
-    $privatePem = file_get_contents($privKeyFile);
-    $publicPem  = file_get_contents($pubKeyFile);
-    $keyDetails = openssl_pkey_get_details(
-        openssl_pkey_get_public($publicPem)
-    );
-    echo "Loaded existing RSA keypair.\n";
-}
+$appWorkvivo = '3981';
+
+/*
+ The Domain associated with the Workvivo app
+*/
+
+$appAud = 'unwired.workvivo.red';
+/*
+       This is the email address of the user you want a one-time passcode
+*/
+
+$userEmail = 'test@nomail';
+
+/*
+       The API Gateway URL for Workvivo, sample code is using api.workvivo.red, for production environments you will need to change this to the appropriate URL for your Workvivo environment.
+
+       EU Production
+       api.workvivo.com
+       api.eu2.workvivo.com
+
+       US Production
+       api.workvivo.us
+       api.us2.workvivo.us
+
+       Middle East Production
+       api.workvivo.me
+
+    */
+
+$apiUrl = 'https://api.workvivo.red/v1/unwired/users/otp'; //In case it is EU Production HOST should be api.workvivo.com
+
+/*
+        this will be the customer’s host typically, e.g. acme.com
+    */
+
+$appIssuer = 'org1';
+
+/*
+        this will always be 'app' as this API is always called as a Applicaton configured in Workvivo that the Partner Application is using to generate OTPs
+    */
+$appSubject = 'app';
+
+$privatePem = file_get_contents($privKeyFile);
+$publicPem = file_get_contents($pubKeyFile);
+$jwks = file_get_contents($jwksFile);
+$jwksData = json_decode(file_get_contents($jwksFile), true);
+$keyDetails = openssl_pkey_get_details(
+    openssl_pkey_get_public($publicPem)
+);
+echo "Loaded existing RSA keypair.\n";
 
 // === UTILITY: base64url ===
 
@@ -64,28 +78,14 @@ function base64url_encode(string $data): string {
     return rtrim(strtr(base64_encode($data), '+/', '-_'), '=');
 }
 
-// === BUILD JWKS ===
-
-$rsa = $keyDetails['rsa'];
-$jwk = [
-    'kty' => 'RSA',
-    'alg' => 'RS256',
-    'use' => 'sig',
-    'n'   => base64url_encode($rsa['n']),
-    'e'   => base64url_encode($rsa['e']),
-];
-$thumbprint = base64url_encode(
-    hash('sha256', json_encode([
-        'e'   => $jwk['e'],
-        'kty' => 'RSA',
-        'n'   => $jwk['n'],
-    ]), true)
-);
-$jwk['kid'] = $thumbprint;
-$jwks = ['keys' => [$jwk]];
+$kid = null;
+if (isset($jwksData['keys'][0]['kid'])) {
+    $kid = $jwksData['keys'][0]['kid'];
+} else {
+    echo "No kid found in JWKS.\n";
+}
 
 // === CREATE JWT ===
-
 $now = time();
 $payload = [
     'jti'          => bin2hex(random_bytes(32)),
@@ -95,7 +95,7 @@ $payload = [
     'aud'          => $appAud,
     'iat'          => $now,
     'nbf'          => $now,
-    'exp'          => $now + $jwtLifetime,
+    'exp'          => $now + 60,
     'state'        => bin2hex(random_bytes(32)),
 ];
 
@@ -103,29 +103,24 @@ $token = JWT::encode(
     $payload,
     $privatePem,
     'RS256',
-    $jwk['kid']
+    $kid
 );
 
 // === OUTPUT ===
 
 echo "\n=== JWT ===\n$token\n";
-echo "\n=== kid ===\n{$jwk['kid']}\n";
+echo "\n=== kid ===\n$kid\n";
 echo "\n=== public.pem ===\n$publicPem\n";
-echo "\n=== jwks ===\n" . json_encode($jwks, JSON_PRETTY_PRINT) . "\n";
-
-// prompt to confirm JWKS is uploaded or public key
-echo "\nPress [Enter] once you have updated your JWKS at:\n"
-   . "https://HOST/admin/developers/apps/manage\n";
-fgets(STDIN);
+echo "\n=== jwks ===\n".json_encode($jwksData, JSON_PRETTY_PRINT)."\n";
 
 // === SEND REQUEST ===
 
 $headers = [
     "Workvivo-Id: $orgId",
     "x-workvivo-jwt: $token",
-    "x-workvivo-jwt-keyid: {$jwk['kid']}",
-    "Accept: application/json",
-    "Content-Type: application/json",
+    "x-workvivo-jwt-keyid: {$kid}",
+    'Accept: application/json',
+    'Content-Type: application/json',
 ];
 $postBody = json_encode(['email' => $userEmail]);
 
